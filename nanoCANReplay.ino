20a6b6d5c04647d24b9493175b9488193a24af2c
bruno <bruno.marelli@gmail.com>
2018-10-07 21:49:54 +0200

    Micros test completed.

--- nanoCANReplay.ino
+++ nanoCANReplay.ino
@@ -1,9 +1,19 @@
+unsigned long time;
 void setup() {
-  // put your setup code here, to run once:
-
+	// put your setup code here, to run once:
+	Serial.begin(115200);
 }
 
+unsigned long previousMicros = micros();
 void loop() {
-  // put your main code here, to run repeatedly:
+	// put your main code here, to run repeatedly:
+	unsigned long now = micros();
+	if ((previousMicros + 1000000) < now + 4)
+	{
+		Serial.print("Time: ");
+		time = micros();
 
+		Serial.println(time);  //prints time since program started
+		previousMicros = now;
+	}
 }
